2d89b58422806f2143ae3ca411a4dc808375ab26
Oleksandr Khomenko <sasha.khomenko12@gmail.com>
2023-08-21 11:55:06 +0300

    initial

---
+++ src/main/java/com/movie/database/ComMovieDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.movie.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComMovieDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComMovieDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movie/database/ComMovieDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComMovieDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
